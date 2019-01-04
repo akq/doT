e850b0e756c0d74db212db7be9e897ae1001bf17
Min Shi <min.shi@thomsonreuters.com>
2019-01-04 14:07:59 +0800

    rename and remove lint dependancy

--- donT.js
+++ donT.js
@@ -1,13 +1,11 @@
-// doT.js
-// 2011-2014, Laura Doktorova, https://github.com/olado/doT
+// donT.js
+// 2018, AKQ, https://github.com/akq/donT
 // Licensed under the MIT license.
-var Linter = require("eslint").Linter;
-var UglifyJS = require("uglify-js");
-(function (Linter, UglifyJS) {
+(function () {
 	"use strict";
 
-	var doT = {
-		name: "doT",
+	var donT = {
+		name: "donT",
 		version: "1.1.1",
 		templateSettings: {
 			evaluate:    /\{\{([\s\S]+?(\}?)+)\}\}/g,
@@ -15,9 +13,7 @@ var UglifyJS = require("uglify-js");
 			attr:        /\{\{@=([\s\S]+?)\}\}/g,
 			urlEnc:        /\{\{\$=([\s\S]+?)\}\}/g,
 			encode:      /\{\{!([\s\S]+?)\}\}/g,
-			// use:         /\{\{#([\s\S]+?)\}\}/g,
 			useParams:   /(^|[^\w$])def(?:\.|\[[\'\"])([\w$\.]+)(?:[\'\"]\])?\s*\:\s*([\w$\.]+|\"[^\"]+\"|\'[^\']+\'|\{[^\}]+\})/g,
-			// define:      /\{\{##\s*([\w\.$]+)\s*(\:|=)([\s\S]+?)#\}\}/g,
 			defineParams:/^\s*([\w$]+):([\s\S]+)/,
 			conditional: /\{\{\?(\?)?\s*([\s\S]*?)\s*\}\}/g,
 			iterate:     /\{\{~\s*(?:\}\}|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*\}\})/g,
@@ -35,7 +31,7 @@ var UglifyJS = require("uglify-js");
 		log: true
 	}, _globals;
 
-	doT.encodeHTMLSource = function(doNotSkipEncoded) {
+	donT.encodeHTMLSource = function(doNotSkipEncoded) {
 		var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", '"': "&#34;", "'": "&#39;", "/": "&#47;" },
 			matchHTML = doNotSkipEncoded ? /[&<>"'\/]/g : /&(?!#?\w+;)|<|>|"|'|\//g;
 		return function(code) {
@@ -47,11 +43,11 @@ var UglifyJS = require("uglify-js");
 
 	/* istanbul ignore else */
 	if (typeof module !== "undefined" && module.exports) {
-		module.exports = doT;
+		module.exports = donT;
 	} else if (typeof define === "function" && define.amd) {
-		define(function(){return doT;});
+		define(function(){return donT;});
 	} else {
-		_globals.doT = doT;
+		_globals.donT = donT;
 	}
 
 	var startend = {
@@ -64,13 +60,12 @@ var UglifyJS = require("uglify-js");
 		return code.replace(/\\('|\\)/g, "$1");//.replace(/[\r\t\n]/g, " ");
 	}
 	function quote(str){	
-		var plains = str.trim().split(doT.templateSettings.tag), arr=[]
+		var plains = str.trim().split(donT.templateSettings.tag), arr=[]
 			, start =-2, end = -1
 			;
 		for(var i=0, len=plains.length; i<len;  i++){
 			var p = plains[i], plain = 0;
 			if(p){
-				var last;
 				if(p == '{{'){
 					start = i;
 				}
@@ -78,15 +73,9 @@ var UglifyJS = require("uglify-js");
 					end = i;
 				}
 				else if(end > start){
-					// arr.push(' ');
 					p = p.replace(/'|\\/g, "\\$&").replace(/\n/g, "\\n").replace(/\r/g, "\\r");
 
-					// var last = arr[arr.length-1];
-					// if(last && last[last.length-1]=='{' && p[0] == '{'){
-					// 	arr.push(' ');
-					// }
 					arr.push(p);
-					// arr.push(' ');
 					plain = 1;
 				}
 				else p = p.replace(/'|\\/g, "\\$&")
@@ -98,16 +87,13 @@ var UglifyJS = require("uglify-js");
 		}
 		return arr.join('');
 	}
-	doT.template = function(tmpl, c, def) {
-		c = c || doT.templateSettings;
-		var cse = c.append ? startend.append : startend.split, needhtmlencode, sid = 0, indv
-			// str  = (c.use || c.define) ? resolveDefs(c, tmpl, def || {}) : tmpl
+	donT.template = function(tmpl) {
+		var c = donT.templateSettings
+			, cse = c.append ? startend.append : startend.split, needhtmlencode, sid = 0, indv
 			, str  =  quote(tmpl)
 			;
-			// console.log('-------',str);	
-		str = ("var out='" + (c.strip ? str.replace(/(^|\r|\n)\t* +| +\t*(\r|\n|$)/g," ")
-					.replace(/\r|\n|\t|\/\*[\s\S]*?\*\//g,""): str)
-			// .replace(/'|\\/g, "\\$&") //quote the single quote and back-slash to 
+		str = ("var out='" + str
+			// .replace(/'|\\/g, "\\$&") //quote the single quote and back-slash 
 			.replace(c.interpolate || skip, function(m, code) {
 				return cse.start + unescape(code) + cse.end;
 			})
@@ -136,44 +122,15 @@ var UglifyJS = require("uglify-js");
 				return "';" + unescape(code) + "out+='";
 			})
 			+ "';return out;");
-			var linter = new Linter();
 			
-			var messages = linter.verifyAndFix(str, {
-				rules: {
-					semi: 2
-				}
-			});
-			
-		str = messages.output
-		// .replace(c.comments, '')	
-		// .replace(/(^|\r|\n)\t* +| +\t*(\r|\n|$)/g," ").replace(/\r|\n|\t|\/\*[\s\S]*?\*\//g,"")
-			// .replace(/\n/g, "\\n").replace(/\t/g, '\\t').replace(/\r/g, "\\r")
+		str = str
 			.replace(/(\s|;|\}|^|\{)out\+='';/g, '$1')
 			.replace(/\+''/g, "");
-			//.replace(/(\s|;|\}|^|\{)out\+=''\+/g,'$1out+=');
-		// if(c.uglify){
-			// var min = UglifyJS.minify(str, {
-			// 	parse: {
-			// 		bare_returns: true,
-			// 		html5_comments: false
-			// 	},
-			// 	compress: false,
-			// 	output: {
-			// 		'inline_script': false
-			// 	}
-			// });
-			// if(min.error){
-			// 	console.log('err, ------------->', str)
-			// 	throw min.error;
-			// }
-			// str = min.code;
-			// console.log('******',str);
-			// console.log(messages.output);
-		// }
+
 		if (needhtmlencode) {
-			if (!c.selfcontained && _globals && !_globals._encodeHTML) _globals._encodeHTML = doT.encodeHTMLSource(c.doNotSkipEncoded);
+			if (!c.selfcontained && _globals && !_globals._encodeHTML) _globals._encodeHTML = donT.encodeHTMLSource(c.doNotSkipEncoded);
 			str = "var encodeHTML = typeof _encodeHTML !== 'undefined' ? _encodeHTML : ("
-				+ doT.encodeHTMLSource.toString() + "(" + (c.doNotSkipEncoded || '') + "));"
+				+ donT.encodeHTMLSource.toString() + "(" + (c.doNotSkipEncoded || '') + "));"
 				+ str;
 		}
 		try {
@@ -185,10 +142,7 @@ var UglifyJS = require("uglify-js");
 		}
 	};
 
-	// doT.compile = function(tmpl, def) {
-	// 	return doT.template(tmpl, null, def);
-	// };
-	doT.compile = function(tmpl) {
-		return doT.template(tmpl);
+	donT.compile = function(tmpl) {
+		return donT.template(tmpl);
 	};
-}(Linter, UglifyJS));
+}());
